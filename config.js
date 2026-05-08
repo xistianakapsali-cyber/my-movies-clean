620648d07a0e8d6eb21f3fa6125a14819ab7969d
xistianakapsali-cyber <xistianakapsali@gmail.com>
2026-05-08 16:34:25 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,18 +4,15 @@
     admin_dashboard_hash: "a8578fc57105ffae6ac94536ccb80f30beecfe32e53c8cb20505d478b52e1ad1",
     
     users: {
-        "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4": "Δημήτρης (Admin)",
-        "7654a4b2e4e8e6f9c4a4e7e6f4f3e2d1c8b7a6f5e4d3c2b1a9f8e7d6c5b4a3f2e1": "Μαρία",
-        "4a2b4c6d8e0f1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7": "Γιάννης"
+        "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4": "Δημήτρης (Admin)"
     },
     
+    // Απλοποιημένη δομή - ΜΟΝΟ public repo
     github: {
         username: "xistianakapsali-cyber",
         repo: "my-movies-clean",
         branch: "main",
-        // Οι φάκελοι μέσα στο repo
-        publicFolder: "my-movies-clean",
-        privateFolder: "my-movies-with-links"
+        folder: "my-movies-clean"  // ← Ο φάκελος μέσα στο repo που έχει τα αρχεία
     },
     
     app_version: "2.0.0"
