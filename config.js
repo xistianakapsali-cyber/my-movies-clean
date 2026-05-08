ec05b60abd65f86fdddc308739800f62befa4160
xistianakapsali-cyber <xistianakapsali@gmail.com>
2026-05-08 17:48:04 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+﻿const YIOIO_CONFIG = {
+    tmdb_api_key: "888d45d72a08e6adad1196a31ef13e85",
+    
+    admin_dashboard_hash: "a8578fc57105ffae6ac94536ccb80f30beecfe32e53c8cb20505d478b52e1ad1",
+    
+    users: {
+        "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4": "Δημήτρης (Admin)"
+    },
+    
+    github: {
+        username: "xistianakapsali-cyber",
+        repo: "my-movies-clean",
+        branch: "main",
+        path: "my-movies-clean"    // ← Ο φάκελος με τα αρχεία
+    },
+    
+    app_version: "2.0.0"
+};
